efb441ef2fbaf95c99bf36255591ea9e5bd05a83
Abhishek suman <abhisheksumancode@gmail.com>
2024-03-26 04:09:52 +0530

    redux persist initialised

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,9 @@ import { Inter } from 'next/font/google'
 import { Provider } from 'react-redux'
 const inter = Inter({ subsets: ['latin'] })
 import store from '../store'
+import { PersistGate } from "redux-persist/integration/react";
+import persistStore from 'redux-persist/es/persistStore';
+const persistor = persistStore(store);
 
 export default function RootLayout({
   children,
@@ -11,11 +14,18 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <Provider store={store}>
-      <html lang="en">
-        <body className={inter.className}>{children}</body>
-      </html>
-    </Provider>
+    <html lang="en">
+      <body>
+
+        <Provider store={store}>
+          <PersistGate persistor={persistor} loading={null}>
+            <html lang="en">
+              <body className={inter.className}>{children}</body>
+            </html>
+          </PersistGate>
+        </Provider>
+      </body>
+    </html>
   )
 }
 
--- store.jsx
+++ store.jsx
@@ -1,11 +1,38 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
 import authSlice from "./app/components/redux/authSlice";
-import postSlice from "./app/components/redux/postSlice";
+import {
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+const persistConfig = {
+  key: "root",
+  storage: AsyncStorage,
+};
+
+const rootReducer = combineReducers({
+  users: authSlice,
+});
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 const store = configureStore({
-  reducer: {
-    users:authSlice,
-  }
+  /*  reducer: {
+    users: authSlice,
+  }, */
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+  getDefaultMiddleware({
+    serializableCheck: {
+      ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+    },
+  }),
 });
 
 export default store;
